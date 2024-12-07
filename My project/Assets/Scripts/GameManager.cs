76954668dfab912a688c542965d0016f06793e61
thaitaigeorgian <200619087@student.georgianc.on.ca>
2024-12-06 22:33:03 -0500

    Add sound, teleport, player wrap, fire bullet,...

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
--- My project/Assets/Scripts/PlayerScript.cs
+++ My project/Assets/Scripts/PlayerScript.cs
@@ -8,24 +8,89 @@ public class PlayerScript : MonoBehaviour
     public float rotationSpeed = 200f;
     private Rigidbody2D rb;
 
+    public AudioClip fireSound;
+    public AudioClip engineSound;
+    public AudioClip explosionSound;
+    public AudioClip teleportSound;
+
+    private AudioSource audioSource;
+
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.forward * -rotation);
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward * -rotation);
+        }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(transform.up * thrustForce);
+            audioSource.PlayOneShot(engineSound);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             TeleportToRandomPosition();
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        CheckWrapAround();
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        audioSource.PlayOneShot(fireSound);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = transform.up * bulletSpeed; // Đạn di chuyển theo hướng "up" của firePoint
+
+            Destroy(bullet, 5f);
+        }
+    }
+
+    void CheckWrapAround()
+    {
+        Vector2 position = transform.position;
+
+        // Nếu vị trí vượt qua giới hạn trục X, chuyển sang phía đối diện
+        if (position.x > 10f)
+        {
+            position.x = -10f;
+        }
+        else if (position.x < -10f)
+        {
+            position.x = 10f;
+        }
+
+        // Nếu vị trí vượt qua giới hạn trục Y, chuyển sang phía đối diện
+        if (position.y > 5f)
+        {
+            position.y = -5f;
+        }
+        else if (position.y < -5f)
+        {
+            position.y = 5f;
+        }
+
+        // Cập nhật vị trí của player
+        transform.position = position;
     }
 
     void TeleportToRandomPosition()
@@ -34,15 +99,18 @@ public class PlayerScript : MonoBehaviour
         float randomY = Random.Range(-5f, 5f);
 
         transform.position = new Vector3(randomX, randomY, transform.position.z);
+        audioSource.PlayOneShot(teleportSound);
 
         Debug.Log($"Teleported to: {randomX}, {randomY}");
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Asteroid"))
         {
-
+            audioSource.PlayOneShot(explosionSound);
+            print("Player and Asteroid");
+            transform.position = new Vector2(0, 0);
         }
     }
 
