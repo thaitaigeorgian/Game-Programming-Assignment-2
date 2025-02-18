266c7be9e653fcd06382ab04a91c6a38e4a9bcf9
BigVinnerz <ifoundaclue1327@hotmail.com>
2025-02-18 13:43:50 -0500

    updated PlayerController script to add flip object

--- My project/Assets/Scripts/Programming 2/Assignment 1 - Animals/PlayerController.cs
+++ My project/Assets/Scripts/Programming 2/Assignment 1 - Animals/PlayerController.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
+    private bool isFacingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +20,18 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.position += (new Vector3(moveX, moveY));
+
+        if ((moveX > 0 && !isFacingRight) || (moveX < 0 && isFacingRight))
+        {
+            FlipObject();
+        }
+    }
+
+    private void FlipObject()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
